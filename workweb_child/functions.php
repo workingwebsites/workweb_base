a524ea61f6a89e2e832e1012bc638598c88b5f6c
Lisa Armstrong <lisa@workingwebsites.ca>
2022-02-18 13:18:21 -0800

    Updating functions.php in child theme

--- workweb_child/functions.php
+++ workweb_child/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Child theme automatically overrides parent
  */
@@ -9,7 +10,8 @@
  * Set what features to show
  * Overrides defaults set in parent.  Pick what you want
  */
-function setwwbFeatures(){
+function setwwbFeatures()
+{
     $GLOBALS['wwbFeatures']['header_image'] = true;
     $GLOBALS['wwbFeatures']['home_box'] = true;
     $GLOBALS['wwbFeatures']['slider'] = true;
@@ -18,26 +20,35 @@ function setwwbFeatures(){
     $GLOBALS['wwbFeatures']['featured_products'] = true;
     $GLOBALS['wwbFeatures']['testimony'] = true;
     $GLOBALS['wwbFeatures']['featured_page'] = true;
+    $GLOBALS['wwbFeatures']['featured_pages'] = true;
     $GLOBALS['wwbFeatures']['modal'] = true;
 
     //What should the 'more' link say?
     $GLOBALS['wwbVars']['more'] = 'more';
-    
+
     //Change to bust style cache
     $GLOBALS['wwbVars']['stylecache'] = wp_get_theme()->get('Version');
 
     //Home Box settings: How many boxes?
     $GLOBALS['wwbVars']['homebox_num'] = 4;
+
+    //Featured Pages settings: How many pages?
+    $GLOBALS['wwbVars']['featuredpages_num'] = 1;
+
+    //Latest Post settings: How many boxes?
+    $GLOBALS['wwbVars']['latestposts_num'] = 1;
 }
 setwwbFeatures();
 
 /**
  * Set page widths
  */
-function workweb_base_primary_sidebar_class(){
-    echo 	"col-md-8 col-lg-7 col-lg-offset-1";
+function workweb_base_primary_sidebar_class()
+{
+    echo     "col-md-8 col-lg-7 col-lg-offset-1";
 }
 
-function workweb_base_sidebar_class(){
-    echo 	"col-md-4 col-lg-3";
+function workweb_base_sidebar_class()
+{
+    echo     "col-md-4 col-lg-3";
 }
