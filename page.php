3d0aca653a8126611d8a541451be747f5fa64e2d
Lisa Armstrong <lisa@workingwebsites.ca>
2022-02-21 12:44:43 -0800

    Cleanup

--- page.php
+++ page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -14,23 +15,23 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area <?php workweb_base_primary_sidebar_class() ?>">
-		<main id="main" class="site-main <?php workweb_base_main_class() ?>" role="main">
-			<?php
-			while ( have_posts() ) : the_post();
+<div id="primary" class="content-area <?php workweb_base_primary_sidebar_class() ?>">
+	<main id="main" class="site-main <?php workweb_base_main_class() ?>" role="main">
+		<?php
+		while (have_posts()) : the_post();
 
-				get_template_part( 'components/page/content', 'page' );
+			get_template_part('components/page/content', 'page');
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+			// If comments are open or we have at least one comment, load up the comment template.
+			if (comments_open() || get_comments_number()) :
+				comments_template();
+			endif;
 
-			endwhile; // End of the loop.
-			?>
+		endwhile; // End of the loop.
+		?>
 
-		</main>
-	</div>
+	</main>
+</div>
 <?php
 get_sidebar();
 get_footer();
