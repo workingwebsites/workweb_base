abe75678fc85595781613c270a2dffd2d55da080
Lisa Armstrong <lisa@workingwebsites.ca>
2022-02-21 10:34:42 -0800

    Adding proper header for Bootstrap

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -9,47 +10,50 @@
  * @package workweb_base
  */
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site container-fluid">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'workweb_base' ); ?></a>
-	<?php if ($GLOBALS['wwbFeatures']['modal'] == true) {
-		 ?>
-	<?php get_template_part( 'inc/modal/template/modal', 'contact' ); ?>
-	<?php } ?>
+	<div id="page" class="site container-fluid">
+		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'workweb_base'); ?></a>
+		<?php if ($GLOBALS['wwbFeatures']['modal'] == true) {
+		?>
+			<?php get_template_part('inc/modal/template/modal', 'contact'); ?>
+		<?php } ?>
 
-	<header class="site-header" role="banner">
-		<div id="masthead"  class="row">
-	    	<div class="col-md-7">
-	    		<?php //get_template_part( 'components/header/header' ); ?>
-				<?php get_template_part( 'components/header/header', 'image' ); ?>
+		<header class="site-header" role="banner">
+			<div id="masthead" class="row">
+				<div class="col-md-7">
+					<?php //get_template_part( 'components/header/header' ); 
+					?>
+					<?php get_template_part('components/header/header', 'image'); ?>
+				</div>
+
+				<aside id="topbar-right" class="col-md-5" role="complementary">
+					<?php if (is_active_sidebar('topbar-right')) { ?>
+						<?php dynamic_sidebar('topbar-right'); ?>
+					<?php }	?>
+				</aside>
 			</div>
 
-			<aside id="topbar-right" class="col-md-5" role="complementary">
-				<?php if ( is_active_sidebar( 'topbar-right' ) ) { ?>
-					<?php dynamic_sidebar( 'topbar-right' ); ?>
-				<?php }	?>
-			</aside>
-		</div>
+			<div id="navigation" class="row">
+				<?php get_template_part('components/navigation/navigation', 'top'); ?>
+			</div>
 
-		<div id="navigation" class="row">
-    		<?php get_template_part( 'components/navigation/navigation', 'top' ); ?>
-		</div>
-
-		<div class="row"><?php custom_breadcrumbs(); ?></div>
+			<div class="row"><?php custom_breadcrumbs(); ?></div>
 
 
-	</header>
+		</header>
 
-	<div id="content" class="site-content row">
+		<div id="content" class="site-content row">
