1e897fc78018717016275d266c0b709ac965c399
Lisa Armstrong <lisa@workingwebsites.ca>
2022-03-03 13:42:15 -0800

    Moving Website by to align right

--- components/footer/site-colophon.php
+++ components/footer/site-colophon.php
@@ -3,7 +3,7 @@
     Copyright
 </div>
 
-<div class="col-md-6 text-right">
+<div class="col-md-6 text-end">
     Website by: <a href="http://workingwebsites.ca" target="_blank">WorkingWebsites</a>
 </div>
 
