7e97f7fe7b018035ce20545406e4412d39174882
Lisa Armstrong <lisa@workingwebsites.ca>
2022-03-03 13:51:10 -0800

    Adding year to copyright

--- components/footer/site-colophon.php
+++ components/footer/site-colophon.php
@@ -1,9 +1,7 @@
-
 <div class="col-md-6">
-    Copyright
+    Copyright <?php echo date("Y") ?>
 </div>
 
 <div class="col-md-6 text-end">
     Website by: <a href="http://workingwebsites.ca" target="_blank">WorkingWebsites</a>
 </div>
-
