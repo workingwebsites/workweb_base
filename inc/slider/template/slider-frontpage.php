80ec20d3730479369210bd11bc2216e95d9c59d4
Lisa Armstrong <lisa@workingwebsites.ca>
2022-05-20 14:58:52 -0700

    Adds not about Slider needs updating

--- inc/slider/template/slider-frontpage.php
+++ inc/slider/template/slider-frontpage.php
@@ -1,5 +1,8 @@
 <?php
-/* REQUIRES HOME BOX */
+
+/**
+ *  NEEDS UPDATING: https://getbootstrap.com/docs/5.1/components/carousel/
+ */
 
 $ar_slider = wws_get_sliders(true);
 
@@ -23,31 +26,31 @@ $ar_slider = wws_get_sliders(true);
 			$title = isset($slider['title']) ? $slider['title'] : NULL;
 			$content = isset($slider['content']) ? wpautop($slider['content']) : NULL;
 			$link = isset($slider['link']) ? '<a href="' . $slider['link'] . '" class="slider_link_a btn btn-sm portfolio_but"">More</a>' : NULL;
-			?>
+		?>
 
-		<div class="carousel-item <?php echo $item_active ?> sliderrow align-items-end" style="background-image:url('<?php echo $image ?>');">
-			<div class="slider_container col-sm-12 offset-md-6 col-md-6 ">
-				<div class="slider">
-					<h2 class="slider_title"><?php echo  $title ?></h2>
-					<div class="slider_content">
-						<?php echo  $content ?>
-						<div class="slider_cta">
-							<?php //echo do_shortcode("[contactmodal title='Get Your Free Estimate']"); 
+			<div class="carousel-item <?php echo $item_active ?> sliderrow align-items-end" style="background-image:url('<?php echo $image ?>');">
+				<div class="slider_container col-sm-12 offset-md-6 col-md-6 ">
+					<div class="slider">
+						<h2 class="slider_title"><?php echo  $title ?></h2>
+						<div class="slider_content">
+							<?php echo  $content ?>
+							<div class="slider_cta">
+								<?php //echo do_shortcode("[contactmodal title='Get Your Free Estimate']"); 
 								?>
+							</div>
+							<div class="slider_link"><?php echo  $link ?></div>
 						</div>
-						<div class="slider_link"><?php echo  $link ?></div>
 					</div>
 				</div>
+				<a class="carousel-control-prev" href="#wwsCarousel" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#wwsCarousel" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
 			</div>
-			<a class="carousel-control-prev" href="#wwsCarousel" role="button" data-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="carousel-control-next" href="#wwsCarousel" role="button" data-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
-		</div>
 		<?php } ?>
 	</div>
 </div>
