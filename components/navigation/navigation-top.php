6fbf43ec76e4e2847d8fd51dcdcda8c82e79ee3c
Lisa Armstrong <lisa@workingwebsites.ca>
2022-05-24 12:17:56 -0700

    Adds collapsable button for menu

--- components/navigation/navigation-top.php
+++ components/navigation/navigation-top.php
@@ -7,7 +7,7 @@
 $arg_full = array(
 	'theme_location' => 'full-top-menu',
 	'depth' => 2,
-	'menu_class' => "navbar-nav",
+	'menu_class' => "navbar-nav collapse navbar-collapse",
 	'container' => 'div',
 	'container_class' => "container_class"
 );
@@ -15,6 +15,9 @@ $arg_full = array(
 
 <nav id="nav_top-menu" class="navbar navbar navbar-expand-lg" role="navigation">
 	<div id="navbarNav" class="col-xs-11">
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu-main-menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span> Menu
+		</button>
 		<?php wp_nav_menu($arg_full); ?>
 	</div>
 </nav>
