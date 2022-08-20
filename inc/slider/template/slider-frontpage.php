d660094da0713877b92f8ea27de1e165408e2b87
Lisa Armstrong <lisa@workingwebsites.ca>
2022-08-19 17:44:32 -0700

    Updates slider to Bootstrap 5.x carousel

--- inc/slider/template/slider-frontpage.php
+++ inc/slider/template/slider-frontpage.php
@@ -1,18 +1,29 @@
 <?php
 
 /**
- *  NEEDS UPDATING: https://getbootstrap.com/docs/5.1/components/carousel/
+ *  Slider / carousel
+ * Displays slides from 'slider' control.
  */
 
 $ar_slider = wws_get_sliders(true);
 
 ?>
-<div id="wwsCarouselSlider" class="carousel slide row" data-ride="carousel">
-	<ol class="carousel-indicators">
-		<li data-target="#wwsCarouselSlider" data-slide-to="0" class="active"></li>
-		<li data-target="#wwsCarouselSlider" data-slide-to="1"></li>
-		<li data-target="#wwsCarouselSlider" data-slide-to="2"></li>
-	</ol>
+
+<div id="wwsCarouselSlider" class="carousel slide" data-bs-ride="carousel">
+
+	<div class="carousel-indicators">
+		<?php
+		for ($i = 0; $i < count($ar_slider); $i++) {
+			if ($i == 0) {	// The first one is active
+				$strParam = 'class="active" aria-current="true" ';
+			} else {
+				$strParam = null;
+			}
+		?>
+			<button type="button" data-bs-target="#wwsCarouselSlider" data-bs-slide-to="<?php echo $i ?>" <?php echo $strParam ?> aria-label="Slide <?php echo $i + 1 ?>"></button>
+		<?php } ?>
+	</div>
+
 	<div class="carousel-inner">
 		<?php
 		$i = -1;
@@ -27,30 +38,24 @@ $ar_slider = wws_get_sliders(true);
 			$content = isset($slider['content']) ? wpautop($slider['content']) : NULL;
 			$link = isset($slider['link']) ? '<a href="' . $slider['link'] . '" class="slider_link_a btn btn-sm portfolio_but"">More</a>' : NULL;
 		?>
-
-			<div class="carousel-item <?php echo $item_active ?> sliderrow align-items-end" style="background-image:url('<?php echo $image ?>');">
-				<div class="slider_container col-sm-12 offset-md-6 col-md-6 ">
-					<div class="slider">
-						<h2 class="slider_title"><?php echo  $title ?></h2>
-						<div class="slider_content">
-							<?php echo  $content ?>
-							<div class="slider_cta">
-								<?php //echo do_shortcode("[contactmodal title='Get Your Free Estimate']"); 
-								?>
-							</div>
-							<div class="slider_link"><?php echo  $link ?></div>
-						</div>
+			<div class="carousel-item <?php echo $item_active ?>">
+				<img src="<?php echo $image ?>" class="d-block w-100" alt="<?php echo $title ?>">
+				<div class="carousel-caption d-none d-md-block">
+					<h5><?php echo $title ?></h5>
+					<div class="slider_content">
+						<?php echo  $content ?>
+						<div class="slider_link"><?php echo  $link ?></div>
 					</div>
 				</div>
-				<a class="carousel-control-prev" href="#wwsCarousel" role="button" data-slide="prev">
-					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span class="sr-only">Previous</span>
-				</a>
-				<a class="carousel-control-next" href="#wwsCarousel" role="button" data-slide="next">
-					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-					<span class="sr-only">Next</span>
-				</a>
 			</div>
 		<?php } ?>
 	</div>
+	<button class="carousel-control-prev" type="button" data-bs-target="#wwsCarouselSlider" data-bs-slide="prev">
+		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		<span class="visually-hidden">Previous</span>
+	</button>
+	<button class="carousel-control-next" type="button" data-bs-target="#wwsCarouselSlider" data-bs-slide="next">
+		<span class="carousel-control-next-icon" aria-hidden="true"></span>
+		<span class="visually-hidden">Next</span>
+	</button>
 </div>
