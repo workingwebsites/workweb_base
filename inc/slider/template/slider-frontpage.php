78f6d79567884cbd9e11605bcc37f262be4c681d
Lisa Armstrong <lisa@workingwebsites.ca>
2022-08-19 17:12:39 -0700

    Copying basic code over

--- inc/slider/template/slider-frontpage.php
+++ inc/slider/template/slider-frontpage.php
@@ -8,82 +8,41 @@ $ar_slider = wws_get_sliders(true);
 
 ?>
 
-<!-- 
-	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="..." alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div> 
--->
-
-<div id="wwsCarouselSlider" class="carousel slide row" data-ride="carousel">
-	<ol class="carousel-indicators">
-		<li data-target="#wwsCarouselSlider" data-slide-to="0" class="active"></li>
-		<li data-target="#wwsCarouselSlider" data-slide-to="1"></li>
-		<li data-target="#wwsCarouselSlider" data-slide-to="2"></li>
-	</ol>
-	<div class="carousel-inner">
-		<?php
-		$i = -1;
-		foreach ($ar_slider as $slider) {
-			++$i;
-			//Make the first one active
-			$item_active = $i == 0 ? 'active' : null;
-
-			//Set vars
-			$image = isset($slider['image'][0]) ? $slider['image'][0] : NULL;
-			$title = isset($slider['title']) ? $slider['title'] : NULL;
-			$content = isset($slider['content']) ? wpautop($slider['content']) : NULL;
-			$link = isset($slider['link']) ? '<a href="' . $slider['link'] . '" class="slider_link_a btn btn-sm portfolio_but"">More</a>' : NULL;
-		?>
-			<div class="carousel-item <?php echo $item_active ?>">
-				<img class="d-block w-100" src="<?php echo $image ?>" alt="First slide">
-			</div>
-
-			<!-- <div class="carousel-item <?php echo $item_active ?> sliderrow align-items-end" style="background-image:url('<?php echo $image ?>');">
-				<div class="slider_container col-sm-12 offset-md-6 col-md-6 ">
-					<div class="slider">
-						<h2 class="slider_title"><?php echo  $title ?></h2>
-						<div class="slider_content">
-							<?php echo  $content ?>
-							<div class="slider_cta">
-								<?php //echo do_shortcode("[contactmodal title='Get Your Free Estimate']"); 
-								?>
-							</div>
-							<div class="slider_link"><?php echo  $link ?></div>
-						</div>
-					</div>
-				</div>
-				<a class="carousel-control-prev" href="#wwsCarousel" role="button" data-slide="prev">
-					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span class="sr-only">Previous</span>
-				</a>
-				<a class="carousel-control-next" href="#wwsCarousel" role="button" data-slide="next">
-					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-					<span class="sr-only">Next</span>
-				</a>
-			</div> -->
-		<?php } ?>
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+	<div class="carousel-indicators">
+		<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+		<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+		<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
 	</div>
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+			<img src="https://aemweb.localhost/wp-content/uploads/2022/05/Photo-0023.jpg" class="d-block w-100" alt="...">
+			<div class="carousel-caption d-none d-md-block">
+				<h5>First slide label</h5>
+				<p>Some representative placeholder content for the first slide.</p>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<img src="https://aemweb.localhost/wp-content/uploads/2022/05/Photo-0029.jpg" class="d-block w-100" alt="...">
+			<div class="carousel-caption d-none d-md-block">
+				<h5>Second slide label</h5>
+				<p>Some representative placeholder content for the second slide.</p>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<img src="https://aemweb.localhost/wp-content/uploads/woocommerce-placeholder-999x999.png" class="d-block w-100" alt="...">
+			<div class="carousel-caption d-none d-md-block">
+				<h5>Third slide label</h5>
+				<p>Some representative placeholder content for the third slide.</p>
+			</div>
+		</div>
+	</div>
+	<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		<span class="visually-hidden">Previous</span>
+	</button>
+	<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+		<span class="carousel-control-next-icon" aria-hidden="true"></span>
+		<span class="visually-hidden">Next</span>
+	</button>
 </div>
