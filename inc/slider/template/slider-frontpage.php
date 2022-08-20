877d1fb300289bf4365a7cfc8ae07f701e8a19d3
Lisa Armstrong <lisa@workingwebsites.ca>
2022-08-19 17:33:40 -0700

    Updating to bootstrap 5.1 carousel

--- inc/slider/template/slider-frontpage.php
+++ inc/slider/template/slider-frontpage.php
@@ -1,47 +1,50 @@
 <?php
 
 /**
- *  NEEDS UPDATING: https://getbootstrap.com/docs/5.1/components/carousel/
+ *  Slider / carousel
  */
 
 $ar_slider = wws_get_sliders(true);
 
 ?>
 
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+<div id="wwsCarouselSlider" class="carousel slide" data-bs-ride="carousel">
 	<div class="carousel-indicators">
-		<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-		<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-		<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+		<button type="button" data-bs-target="#wwsCarouselSlider" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+		<button type="button" data-bs-target="#wwsCarouselSlider" data-bs-slide-to="1" aria-label="Slide 2"></button>
+		<button type="button" data-bs-target="#wwsCarouselSlider" data-bs-slide-to="2" aria-label="Slide 3"></button>
 	</div>
 	<div class="carousel-inner">
-		<div class="carousel-item active">
-			<img src="https://aemweb.localhost/wp-content/uploads/2022/05/Photo-0023.jpg" class="d-block w-100" alt="...">
-			<div class="carousel-caption d-none d-md-block">
-				<h5>First slide label</h5>
-				<p>Some representative placeholder content for the first slide.</p>
+		<?php
+		$i = -1;
+		foreach ($ar_slider as $slider) {
+			++$i;
+			//Make the first one active
+			$item_active = $i == 0 ? 'active' : null;
+
+			//Set vars
+			$image = isset($slider['image'][0]) ? $slider['image'][0] : NULL;
+			$title = isset($slider['title']) ? $slider['title'] : NULL;
+			$content = isset($slider['content']) ? wpautop($slider['content']) : NULL;
+			$link = isset($slider['link']) ? '<a href="' . $slider['link'] . '" class="slider_link_a btn btn-sm portfolio_but"">More</a>' : NULL;
+		?>
+			<div class="carousel-item <?php echo $item_active ?>">
+				<img src="<?php echo $image ?>" class="d-block w-100" alt="<?php echo $title ?>">
+				<div class="carousel-caption d-none d-md-block">
+					<h5><?php echo $title ?></h5>
+					<div class="slider_content">
+						<?php echo  $content ?>
+						<div class="slider_link"><?php echo  $link ?></div>
+					</div>
+				</div>
 			</div>
-		</div>
-		<div class="carousel-item">
-			<img src="https://aemweb.localhost/wp-content/uploads/2022/05/Photo-0029.jpg" class="d-block w-100" alt="...">
-			<div class="carousel-caption d-none d-md-block">
-				<h5>Second slide label</h5>
-				<p>Some representative placeholder content for the second slide.</p>
-			</div>
-		</div>
-		<div class="carousel-item">
-			<img src="https://aemweb.localhost/wp-content/uploads/woocommerce-placeholder-999x999.png" class="d-block w-100" alt="...">
-			<div class="carousel-caption d-none d-md-block">
-				<h5>Third slide label</h5>
-				<p>Some representative placeholder content for the third slide.</p>
-			</div>
-		</div>
+		<?php } ?>
 	</div>
-	<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+	<button class="carousel-control-prev" type="button" data-bs-target="#wwsCarouselSlider" data-bs-slide="prev">
 		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 		<span class="visually-hidden">Previous</span>
 	</button>
-	<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+	<button class="carousel-control-next" type="button" data-bs-target="#wwsCarouselSlider" data-bs-slide="next">
 		<span class="carousel-control-next-icon" aria-hidden="true"></span>
 		<span class="visually-hidden">Next</span>
 	</button>
