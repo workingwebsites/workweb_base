2c03d7cff44f1c775e6ae80cb35012993341a7c5
Lisa Armstrong <lisa@workingwebsites.ca>
2022-09-13 08:38:14 -0700

    Removes '0' coming up in homeboxes if no image

--- inc/home-boxes/template/homebox-frontpage.php
+++ inc/home-boxes/template/homebox-frontpage.php
@@ -1,20 +1,19 @@
 <?php
 /* REQUIRES HOME BOX */
 
-	$ar_homebox = wws_get_homebox();
+$ar_homebox = wws_get_homebox();
 
-	$i = -1;	// To set first one at 0
-	$mdNum = 3;
+$i = -1;	// To set first one at 0
+$mdNum = 3;
 
-	foreach ( $ar_homebox As $homebox) {
-		++$i;
+foreach ($ar_homebox as $homebox) {
+	++$i;
 
-		//Set vars
-		$image = isset( $homebox['image'] )? $homebox['image'] : NULL;
-		$title = isset( $homebox['title'] )? $homebox['title'] : NULL;
-		$content = isset( $homebox['content'] )? wpautop( $homebox['content'] ) : NULL;
-		$link = isset( $homebox['link'] )?'<a href="'.$homebox['link'].'" class="homebox_link_a">More</a>': NULL;
-
+	//Set vars
+	$image = isset($homebox['image']) && $homebox['image'] != 0 ? $homebox['image'] : NULL;
+	$title = isset($homebox['title']) ? $homebox['title'] : NULL;
+	$content = isset($homebox['content']) ? wpautop($homebox['content']) : NULL;
+	$link = isset($homebox['link']) ? '<a href="' . $homebox['link'] . '" class="homebox_link_a">More</a>' : NULL;
 ?>
 	<div id="homebox_<?php echo $i ?>" class="col-md-<?php echo $mdNum ?> homebox_container">
 		<div class="homebox">
@@ -24,6 +23,6 @@
 			<div class="homebox_link"><?php echo  $link ?></div>
 		</div>
 	</div>
- <?php
+<?php
 }
 ?>
